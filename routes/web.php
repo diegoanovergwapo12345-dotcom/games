c28a1d3117d992c4ab34b059d820468a747f9d21
louisediego6-crypto <diegoanovergwapo12345@gmail.com>
2026-05-30 14:36:18 +0800

    Set login as default route

--- routes/web.php
+++ routes/web.php
@@ -10,24 +10,27 @@ use App\Http\Controllers\GameController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ContactController;
 
-Route::get('/landing',  fn () => view('landing'))->name('landing');
-Route::get('/about',    fn () => view('about'))->name('about');
+Route::redirect('/', '/login');
+
+Route::get('/landing', fn () => view('landing'))->name('landing');
+Route::get('/about', fn () => view('about'))->name('about');
 Route::get('/features', fn () => view('features'))->name('features');
 
 Route::get('/games', [GameController::class, 'collection'])->name('games.collection');
 
-Route::get('/contacts',  [ContactController::class, 'index'])->name('contacts.index');
+Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
 Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
 
-Route::get('/register',  [AuthController::class, 'showRegister'])->name('register.show');
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register.show');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 
-Route::get('/login',  [AuthController::class, 'showLogin'])->name('login');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.store');
 
 Route::get('/debug-avatar', function () {
     $user = Auth::user()->fresh();
     $path = 'uploads/' . $user->avatar;
+
     return response()->json([
         'avatar_field'        => $user->avatar,
         'avatar_url'          => $user->avatar_url,
@@ -51,17 +54,17 @@ Route::middleware('auth')->group(function () {
         return redirect('/login');
     })->name('logout');
 
-    Route::get('/dashboard',           [DashboardController::class, 'index'])->name('dashboard.index');
-    Route::post('/dashboard',          [DashboardController::class, 'store'])->name('dashboard.store');
-    Route::put('/dashboard/{user}',    [DashboardController::class, 'update'])->name('dashboard.update');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::post('/dashboard', [DashboardController::class, 'store'])->name('dashboard.store');
+    Route::put('/dashboard/{user}', [DashboardController::class, 'update'])->name('dashboard.update');
     Route::delete('/dashboard/{user}', [DashboardController::class, 'destroy'])->name('dashboard.destroy');
 
-    Route::get('/profile',                 [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::put('/profile',                 [ProfileController::class, 'update'])->name('profile.update');
-    Route::post('/profile/photo/update',   [ProfileController::class, 'updatePhoto'])->name('profile.photo.update');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::post('/profile/photo/update', [ProfileController::class, 'updatePhoto'])->name('profile.photo.update');
     Route::delete('/profile/photo/remove', [ProfileController::class, 'removePhoto'])->name('profile.photo.remove');
 
-    Route::get('/analytics',     [AnalyticsController::class, 'index'])->name('analytics');
+    Route::get('/analytics', [AnalyticsController::class, 'index'])->name('analytics');
 
     Route::get('/gamecollection', fn () => view('gamecollection'))->name('gamecollection');
 
