713161470c76801f0b97dd303c465a52ad48d1d2
louisediego6-crypto <diegoanovergwapo12345@gmail.com>
2026-06-03 17:46:30 +0800

    Force HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(\App\Http\Middleware\UpdateLastSeen::class);
-    })
+    ->withMiddleware(function ($middleware) {
+    $middleware->trustProxies(at: '*');
+})
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
